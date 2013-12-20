7efd92e736efec85c4b02dbd94d6c54e8630f749
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2013-12-20 15:13:26 +0100

    rec.py also writes histograms into a file

--- rec.py
+++ rec.py
@@ -4,6 +4,22 @@ if not ROOT.gROOT.ProcessLine(".L rec.C+")==0:
    print "Error loading/compiling rec_C.so Exiting."
    sys.exit()
 
-myRec = ROOT.rec("/shome/gregor/testbeam_data/Run7.root")
+basepath = "/shome/gregor/testbeam_data/"
+run      = "Run7"
+
+
+myRec = ROOT.rec( basepath + run + ".root")
 myRec.Loop()
 
+li_hs = [ROOT.histo,
+         ROOT.histo1,
+         ROOT.histo2,
+         ROOT.histo3]
+
+outfile = ROOT.TFile( basepath+run + "_histos.root", "RECREATE" )
+outfile.cd()
+
+for h in li_hs:
+    h.Write()
+
+outfile.Close()
