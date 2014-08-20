0bf1071c520118cc94978dd6d62a275d2c5e596a
Dmitry Hits <dmitry.hits@gmail.com>
2014-08-20 11:47:12 +0200

    adding a script that creates an SQL database of July runs from a csv file

--- create_database_of_runs.py
+++ create_database_of_runs.py
@@ -1,12 +1,31 @@
 import sqlite3
+import csv
+
 conn = sqlite3.connect('runs_july2014.db')
 
 c = conn.cursor()
 c.execute('''CREATE TABLE runs
-             (diamond text, run_type text, rate text, voltage text, duration text, comments text)''')
+             (run INT, diamond TEXT, run_type TEXT, rate TEXT, voltage TEXT, duration TEXT, comments TEXT)''')
 
-# Insert a row of data
-c.execute("INSERT INTO runs VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+csvfile = open('run_summary_for_july_testbeam.csv', 'rU')
+csvreader = csv.reader(csvfile, delimiter=',')
+row_number = 0
+for row in csvreader:
+    if row_number>0:
+#        print row
+        type = ""
+        if row[2] == "yes":
+            type = "SIGNAL" 
+        elif row[3] == "yes":
+            type = "PEDESTAL"
+        else:
+            print "ERROR: for run number " + row[0]+  " the data neither signal nor pedestal"
+        new_row = [int(row[0]),row[1], type, row[4],row[5], row[6], row[7]]
+        # Insert a row of data
+        c.execute("INSERT INTO runs VALUES (?,?,?,?,?,?,?)",new_row)
+
+    row_number+=1
+
 
 # Save (commit) the changes
 conn.commit()
